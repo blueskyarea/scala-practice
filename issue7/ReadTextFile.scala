53dab4994c817d0a7230aa8ff8ec330daf949b4b
mh <mh@dummy.ne.jp>
2014-10-10 23:51:05 +0900

    issue7 - コードフォーマット

--- issue7/ReadTextFile.scala
+++ issue7/ReadTextFile.scala
@@ -3,7 +3,7 @@ import scala.io.Source
 
 object ReadTextFile {
   def main(args: Array[String]): Unit = {
-    if(!checkArgs(args)) return
+    if (!checkArgs(args)) return
     val num = args(0).toInt
     readText(num)
   }
@@ -15,7 +15,9 @@ object ReadTextFile {
     } else if (args(0) == "0") {
       println("1以上の数字を指定してください。")
       false
-    } else if (args(0) forall {_.isDigit}) {
+    } else if (args(0) forall {
+      _.isDigit
+    }) {
       true
     } else {
       println("引数が数字ではありません。")
@@ -31,9 +33,9 @@ object ReadTextFile {
       textFile.close
     }
     val listSize = list.size
-    if(num > listSize){
+    if (num > listSize) {
       println(listSize + "より小さい値を指定してください。")
-    }else{
+    } else {
       println(list(num - 1))
     }
   }
